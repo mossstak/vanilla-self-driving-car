0b668170f9189d00425b6929cb4f1c94f9f5bdd6
Mostak Khan <Mostak1993@gmail.com>
2023-09-24 15:38:39 +0100

    adding files

--- car.js
+++ car.js
@@ -12,8 +12,11 @@ class Car {
     this.angle = 0;
     this.damaged = false;
 
+    this.useBrain = controlType == "AI";
+
     if (controlType != "DUMMY") {
       this.sensor = new Sensor(this);
+      this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 5, 4,]);
     }
     this.controls = new Controls(controlType);
   }
@@ -26,6 +29,18 @@ class Car {
     }
     if (this.sensor) {
       this.sensor.update(roadBorders, traffic);
+      const offsets = this.sensor.readings.map((s) =>
+        s == null ? 0 : 1 - s.offset
+      );
+      const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+      console.log(outputs);
+
+      if(this.useBrain){
+        this.controls.forward = outputs[0];
+        this.controls.left = outputs[1];
+        this.controls.right = outputs[2];
+        this.controls.reverse = outputs[3];
+      }
     }
   }
 
@@ -108,7 +123,7 @@ class Car {
     this.y -= Math.cos(this.angle) * this.speed;
   }
 
-  draw(ctx, color) {
+  draw(ctx, color, drawSensor=false) {
     // ctx.save();
     // ctx.translate(this.x, this.y);
     // ctx.rotate(-this.angle);
@@ -122,7 +137,7 @@ class Car {
     if (this.damaged) {
       ctx.fillStyle = "gray";
     } else {
-      ctx.fillStyle=color;
+      ctx.fillStyle = color;
     }
 
     ctx.beginPath();
@@ -131,8 +146,8 @@ class Car {
       ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
     }
     ctx.fill();
-    
-    if(this.sensor){
+
+    if (this.sensor && drawSensor) {
       this.sensor.draw(ctx);
     }
   }
