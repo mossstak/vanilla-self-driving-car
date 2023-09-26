cdb976457d4258575dbad88c4ce0a2ee00b29193
Mostak Khan <Mostak1993@gmail.com>
2023-09-26 21:47:41 +0100

    adding files

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const networkCtx = networkCanvas.getContext("2d");
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 
-const N = 1000;
+const N = 100;
 const cars = generateCars(N)
 let bestCar = cars[0];
 if(localStorage.getItem("bestBrain")){
@@ -23,14 +23,14 @@ if(localStorage.getItem("bestBrain")){
   }
 }
 const traffic = [
-    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
-    new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
-    new Car(road.getLaneCenter(3),  300, 30, 50, "DUMMY", 2),
-    new Car(road.getLaneCenter(1),  300, 30, 50, "DUMMY", 2),
-    new Car(road.getLaneCenter(2),  500, 30, 50, "DUMMY", 2),
-    new Car(road.getLaneCenter(1),  400, 30, 50, "DUMMY", 2),
-    new Car(road.getLaneCenter(3),  100, 30, 50, "DUMMY", 2),
-    new Car(road.getLaneCenter(2),  400, 30, 50, "DUMMY", 2),
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2, getRandomColor()),
+    new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2, getRandomColor()),
+    new Car(road.getLaneCenter(3),  300, 30, 50, "DUMMY", 2, getRandomColor()),
+    new Car(road.getLaneCenter(1),  300, 30, 50, "DUMMY", 2, getRandomColor()),
+    new Car(road.getLaneCenter(2),  500, 30, 50, "DUMMY", 2, getRandomColor()),
+    new Car(road.getLaneCenter(1),  400, 30, 50, "DUMMY", 2, getRandomColor()),
+    new Car(road.getLaneCenter(3),  100, 30, 50, "DUMMY", 2, getRandomColor()),
+    new Car(road.getLaneCenter(2),  400, 30, 50, "DUMMY", 2, getRandomColor()),
     
 ];
 
@@ -77,16 +77,16 @@ function animate(time) {
 
   road.draw(carCtx);
   for (let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(carCtx, "red");    
+    traffic[i].draw(carCtx);    
   }
 
   carCtx.globalAlpha = 0.2;
   for (let i = 0; i < cars.length; i++){
-    cars[i].draw(carCtx, "blue");
+    cars[i].draw(carCtx);
   }
   
   carCtx.globalAlpha = 1;
-  bestCar.draw(carCtx, "blue", true);
+  bestCar.draw(carCtx, true);
 
 
   carCtx.restore();
